17cf3403ccbc66d355df0909245308ff3708412a
Shadaw <blackshadowm1379@gmail.com>
2018-10-15 12:33:24 -0300

    add fake shortener

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 let shortUrl = document.querySelectorAll('.shortUrl');
 let hits = document.querySelectorAll('.hits');
+let input = document.getElementById('header__content--input');
+let button = document.getElementsByClassName('header__content--button')[0];
+let excluir = document.getElementById('header__content--delete');
 
-axios.get('https://raw.githubusercontent.com/chaordic/frontend-intern-challenge/master/Assets/urls.json')
-  .then(function(response) {
+function url() {
+  return axios.get('https://raw.githubusercontent.com/chaordic/frontend-intern-challenge/master/Assets/urls.json')
+}
+dados = url()
+
+dados.then(function(response) {
     for(let i = 0; i <= 4; i++){
       shortUrl[i].innerHTML = response.data[i].shortUrl;
       hits[i].innerHTML = response.data[i].hits;
@@ -11,3 +18,37 @@ axios.get('https://raw.githubusercontent.com/chaordic/frontend-intern-challenge/
   .catch(function(error) {
     console.log(error);
   })
+  
+let valor = 0;
+
+button.onclick = function() {
+  if (valor % 2 == 0) {
+    dados.then(function(response) {
+      for(let i = 0; i <= response.data.length; i++){
+        if(input.value === response.data[i].url) {
+          input.value = response.data[i].shortUrl
+        }
+      }
+    })
+    input.classList.add('--copy')
+    button.value = 'COPIAR'
+    valor ++
+  }
+  else {
+    input.select()
+    document.execCommand('copy')
+    input.classList.remove('--copy')
+    input.value = ''
+    button.value = 'ENCURTAR'
+    valor ++
+  }
+  excluir.classList.toggle('disabled');
+}
+
+excluir.onclick = function() {
+  input.value = ''
+  button.value = 'ENCURTAR'
+  input.classList.remove('--copy')
+  excluir.classList.toggle('disabled');
+  valor ++
+}
